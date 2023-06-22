de83dc0d08adaa8304d115193a18e9b0e71eb223
Neil Fulcher <neilfulcher@e.com>
2023-06-22 20:00:54 +0100

    Initial Commit

---
+++ SailRace/SailRaceApp.swift
@@ -0,0 +1,20 @@
+//
+//  SailRaceApp.swift
+//  SailRace
+//
+//  Created by Neil Fulcher on 22/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SailRaceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
